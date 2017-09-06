0f9ffe83ae3e31691102f7792381fac709f454a6
donaldcaruana <31704391+donaldcaruana@users.noreply.github.com>
2017-09-06 13:28:07 -0400

    Update zen23.groovy

--- devicetypes/donaldcaruana/zen23.src/zen23.groovy
+++ devicetypes/donaldcaruana/zen23.src/zen23.groovy
@@ -4,9 +4,9 @@
  */
 
 metadata {
-	definition (name: "zen23", namespace: "donaldcaruana", author: "Donald Caruana") {
-		capability "Switch"
-		capability "Refresh"
-	}
+    definition (name: "zen23", namespace: "donaldcaruana", author: "Donald Caruana") {
+    capability "Switch"
+    capability "Refresh"
+    }
 }
 //End of file
