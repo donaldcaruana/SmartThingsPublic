65a3d2bfc8ffa6a7a8f261a74bfca113b4e9787b
donaldcaruana <31704391+donaldcaruana@users.noreply.github.com>
2017-09-06 13:12:45 -0400

    Create zen23.groovy

---
+++ devicetypes/donaldcaruana/zen23.src/zen23.groovy
@@ -0,0 +1,12 @@
+/**
+ *  Trying to get something to sync
+ *  So now what?
+ */
+
+metadata {
+	definition (name: "zen23", namespace: "donaldcaruana", author: "Donald Caruana") {
+		capability "Switch"
+		capability "Refresh"
+	}
+}
+//End of file
