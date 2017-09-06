7f32015e11cea787401318e60578ed70f2081cca
donaldcaruana <31704391+donaldcaruana@users.noreply.github.com>
2017-09-06 13:44:37 -0400

    Create testing2.groovy

---
+++ devicetypes/donaldcaruana/testing2.src/testing2.groovy
@@ -0,0 +1,15 @@
+/**
+ *  Trying to get something to sync
+ *  So now what?
+ */
+
+metadata {
+	definition (name: "testing2", namespace: "donaldcaruana", author: "Donald Caruana") {
+		capability "Switch"
+		capability "Refresh"
+	}
+}
+
+def ping() {
+	refresh()
+}
